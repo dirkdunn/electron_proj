5c78795cdd73b611466dc76d5b07cc0cdf5ce7ba
dirkdunn <dirkdunn1@gmail.com>
2017-07-20 10:53:53 -0500

    finish hand detection

--- scratchpad.js
+++ scratchpad.js
@@ -38,6 +38,17 @@ PokerHand.prototype.findHand = function(hand){
   }
 }
 
+PokerHand.prototype.getNumericalHand = function(hand){
+  return hand.split(/\s/).map(function(card){
+    if(card.length === 3){
+      return cardValues[card[0]+card[1]]
+    } else {
+      return cardValues[card[0]]
+    }
+  }).sort();
+}
+
+// Has functions return bool.
 PokerHand.prototype.hasFlush = function(hand){
   var cardSum = "";
   this.hand.split(/\s/).forEach(function(card){
@@ -49,19 +60,9 @@ PokerHand.prototype.hasFlush = function(hand){
 
   })
 
-  // console.log(/^(.)\1+$/.test(cardSum))
+  //console.log(/^(.)\1+$/.test(cardSum))
   return /^(.)\1+$/.test(cardSum);
 
-}
-
-PokerHand.prototype.getNumericalHand = function(hand){
-  return hand.split(/\s/).map(function(card){
-    if(card.length === 3){
-      return cardValues[card[0]+card[1]]
-    } else {
-      return cardValues[card[0]]
-    }
-  }).sort();
 }
 
 PokerHand.prototype.hasStraight = function(){
@@ -74,19 +75,18 @@ PokerHand.prototype.hasStraight = function(){
     }
   }
 
-  console.log(isStraight)
+  //console.log(isStraight)
   return isStraight;
 }
 
-// Has functions return bool.
 PokerHand.prototype.hasRoyalFlush = function(hand){
   var hasAce = this.hand.indexOf('A') != -1;
   var hasKing = this.hand.indexOf('K') != -1;
   var hasQueen = this.hand.indexOf('Q') != -1;
   var hasJack = this.hand.indexOf('J') != -1;
   var hasTen = this.hand.indexOf('10') != -1;
-
-  if(hasAce && hasKing && hasQueen && hasJack && hasTen && this.hasFlush(hand)){
+  //  && this.hasFlush(hand)
+  if(hasAce && hasKing && hasQueen && hasJack && hasTen){
     return true;
   } else {
     return false;
@@ -99,30 +99,47 @@ PokerHand.prototype.hasStraightFlush = function(hand){
 }
 
 PokerHand.prototype.hasFourOfAKind = function(hand){
-  var numericalHand = this.getNumericalHand(hand).join('');
-  return /(.)\1{3}/.test(numericalHand);
+  var numericalHand = this.getNumericalHand(this.hand).join(' ') + ' ';
+  // console.log('num hand: ', numericalHand)
+  return /(\d|\w+\s?)\1{3}/.test(numericalHand);
 }
 
-PokerHand.prototype.hasFullHouse = function(hand){
-  var numericalHand = this.getNumericalHand(hand).join('');
-  var hasThreeOfAKind = /(.)\1{2}/.test(numericalHand);
+PokerHand.prototype.hasFullHouse = function(){
+  var numericalHand = this.getNumericalHand(this.hand).join(' ') + ' ';
+  var hasThreeOfAKind = /(\d|\w+\s?)\1{2}/.test(numericalHand);
+  var fullHouse = false;
 
   if(hasThreeOfAKind){
-    var otherTwoCards = numericalHand.replace(/(.)\1{2}/,'');
-    if()
-  } else{
-    return false;
+    var otherTwoCards = numericalHand.replace(/(\d|\w+\s?)\1{2}/,'') + ' ';
+    var hasPair = /(\d|\w+\s?)\1{1}/.test(otherTwoCards);
+    // console.log('otherTwoCards: ',otherTwoCards, 'hasPair: ',hasPair)
+    if(hasPair){
+      fullHouse = true;
+    }
   }
+
+  return fullHouse;
 }
 
-PokerHand.prototype.hasThreeOfAKind = function(hand){
-  var numericalHand = this.getNumericalHand(hand).join('');
-  return /(.)\1{2}/.test(numericalHand);
+PokerHand.prototype.hasThreeOfAKind = function(){
+  var numericalHand = this.getNumericalHand(this.hand).join(' ') + ' ';
+  // console.log('num hand: ', numericalHand)
+  return /(\d|\w+\s?)\1{2}/.test(numericalHand);
 }
 
-PokerHand.prototype.hasTwoPairs = function(hand){}
+PokerHand.prototype.hasTwoPairs = function(hand){
+  var numericalHand = this.getNumericalHand(this.hand).join(' ') + ' ';
+  var twoPair = numericalHand.match(/(\d+\s?)\1{1}/g).length == 2;
 
-PokerHand.prototype.hasPair = function(hand){}
+  return twoPair;
+}
+
+PokerHand.prototype.hasPair = function(hand){
+  var numericalHand = this.getNumericalHand(this.hand).join(' ') + ' ';
+  var pair = numericalHand.match(/(\d+\s?)\1{1}/g).length == 1;
+
+  return pair;
+}
 
 PokerHand.prototype.compareWith = function(hand){
     return Result.tie;
@@ -130,5 +147,11 @@ PokerHand.prototype.compareWith = function(hand){
 
 var test = new PokerHand("10H JH QH KH AH")
 // test.hasFlush()
-test.hasStraight()
-test.hasStraightFlush();
+// test.hasStraight()
+// test.hasStraightFlush();
+var test2 = new PokerHand("10H 9H 10H 10H AH")
+console.log('TOAK: ',test2.hasThreeOfAKind())
+console.log('FOAK: ', test2.hasFourOfAKind())
+console.log('FH', test2.hasFullHouse())
+console.log('TP: ',test2.hasTwoPairs())
+console.log('P: ', test2.hasPair())
