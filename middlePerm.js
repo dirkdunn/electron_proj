f877a24542a263a3982b01486634e1700a902669
dirkdunn <dirkdunn1@gmail.com>
2017-07-23 20:59:59 -0500

    updates middleperm

--- middlePerm.js
+++ middlePerm.js
@@ -4,6 +4,7 @@ function middlePermutation(s) {
   //coding and coding..
   var randomizations = getRandomizations(s,getFactorial(s));
   console.log(randomizations);
+
   function getFactorial(s){
     var sLength = s.length;
     var factorial = 1;
@@ -15,28 +16,21 @@ function middlePermutation(s) {
   }
 
   function getRandomizations(s, nOutcomes){
-    var outcomes = [s];
-    console.log(nOutcomes)
-
-    // while(outcomes.length < nOutcomes){
-    for(var o=0;o<1000;o++){
-      var blenderArray = [];
-      var toBeSpliced = s.split('');
-      for(var i=0;i<toBeSpliced.length;i++){
-        var incisionPoint = Math.floor(Math.random() * toBeSpliced.length);
-        blenderArray = blenderArray.concat(toBeSpliced.splice(incisionPoint,1));
-      }
-      blenderArray = blenderArray.concat(toBeSpliced);
-      var randomVariation = blenderArray.join('');
-
-      if(outcomes.indexOf(randomVariation) == -1){
-        outcomes.push(randomVariation)
-      }
-      // console.log(outcomes);
+    var outcomes = [s,s.split('').reverse().join('')];
+    var rotate = function(sequence){
+      sequence = sequence.split('');
+      sequence.push(sequence.shift());
+      return sequence.join('');
     }
-  //  }
+    var currentRotation = rotate(s);
 
-  return outcomes.sort();
+    do {
+      outcomes.push(currentRotation);
+      outcomes.push(currentRotation.split('').reverse().join(''));
+      currentRotation = rotate(currentRotation)
+    } while(currentRotation != s)
+
+  return outcomes;
   }
 
 
