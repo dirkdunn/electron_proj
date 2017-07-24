a7595738db88fe7108f0b2ba5f3b2fb23839d6eb
dirkdunn <dirkdunn1@gmail.com>
2017-07-24 14:52:49 -0500

    add midle rotation

--- middlePerm.js
+++ middlePerm.js
@@ -3,6 +3,7 @@ console.log(middlePermutation(process.argv[2]))
 function middlePermutation(s) {
   //coding and coding..
   var randomizations = getRandomizations(s,getFactorial(s));
+
   console.log(randomizations);
 
   function getFactorial(s){
@@ -17,20 +18,40 @@ function middlePermutation(s) {
 
   function getRandomizations(s, nOutcomes){
     var outcomes = [s,s.split('').reverse().join('')];
-    var rotate = function(sequence){
+    var rotate = function(sequence,i){
       sequence = sequence.split('');
-      sequence.push(sequence.shift());
+      sequence.push(sequence.splice(i,i+1)[0]);
       return sequence.join('');
     }
-    var currentRotation = rotate(s);
+    var index = 0;
+    var currentRotation = rotate(s,index);
 
     do {
-      outcomes.push(currentRotation);
-      outcomes.push(currentRotation.split('').reverse().join(''));
-      currentRotation = rotate(currentRotation)
+      var reverseRotation = currentRotation.split('').reverse().join('');
+      if(outcomes.indexOf(currentRotation) == -1){
+        outcomes.push(currentRotation);
+      }
+      if(outcomes.indexOf(reverseRotation) == -1){
+        outcomes.push(reverseRotation);
+      }
+
+      // Rotate the rest of the sequence
+      var innerRotation = currentRotation;
+      for(var i=index;i<s.length;i++){
+        innerRotation = rotate(innerRotation,i);
+        var reverseInnerRotation = innerRotation.split('').reverse().join('');
+        if(innerRotation.length === s.length && (outcomes.indexOf(innerRotation) == -1)){
+          outcomes.push(innerRotation);
+          if(outcomes.indexOf(reverseInnerRotation) == -1){
+            outcomes.push(reverseInnerRotation);
+          }
+        }
+      }
+
+      currentRotation = rotate(currentRotation,index)
     } while(currentRotation != s)
 
-  return outcomes;
+  return outcomes.sort();
   }
 
 
